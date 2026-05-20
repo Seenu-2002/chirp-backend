f2be3e2af64ea69011e1ef3c85766d60db036f65
Seenu-2002 <misterseenivasan2002@gmail.com>
2026-05-20 11:08:09 +0530

    - Added token refresh endpoint and logout endpoint

--- user/src/main/kotlin/com/seenu/dev/chirp/user/api/controller/AuthController.kt
+++ user/src/main/kotlin/com/seenu/dev/chirp/user/api/controller/AuthController.kt
@@ -2,11 +2,13 @@ package com.seenu.dev.chirp.user.api.controller
 
 import com.seenu.dev.chirp.user.api.dto.AuthenticatedUserDto
 import com.seenu.dev.chirp.user.api.dto.LoginRequest
+import com.seenu.dev.chirp.user.api.dto.RefreshRequest
 import com.seenu.dev.chirp.user.api.dto.RegistrationRequest
 import com.seenu.dev.chirp.user.api.dto.UserDto
 import com.seenu.dev.chirp.user.api.mapper.toDto
 import com.seenu.dev.chirp.user.service.AuthService
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -35,6 +37,22 @@ class AuthController constructor(
             email = body.email,
             password = body.password
         ).toDto()
+    }
+
+    @PostMapping("/refresh")
+    fun refresh(
+        @RequestBody body: RefreshRequest
+    ): AuthenticatedUserDto {
+        return authService
+            .refresh(body.refreshToken)
+            .toDto()
+    }
+
+    @PostMapping("/logout")
+    fun logout(
+        @RequestBody body: RefreshRequest // Reusing @RefreshRequest as it has the same structure
+    ) {
+        authService.logout(body.refreshToken)
     }
 
 }
